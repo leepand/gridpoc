4c476cd0241a95a4a7d2abf53a519d3749ecfb94
leepand <85721094@qq.com>
2019-03-11 10:01:50 +0800

    first commit

--- core/controller/abtesting/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: UTF-8 -*-
-# author: Leepand
-# time: 2019-01-25
-# desc: Arthur project refactoring
--- core/controller/deploy/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-# author: Leepand
-# time: 2019-01-25
-# desc: Arthur project refactoring
-
-__version__='2.0.1'
